a2b000ef2ac4bfcf9452778794f9fb6df42a161c
halilibrahimozturk <halilibraahimozturk@gmail.com>
2025-05-02 23:57:16 +0300

    Added simple use of library Live Football World Cup Score Board into main method

--- src/main/java/org/ozturk/Main.java
+++ src/main/java/org/ozturk/Main.java
@@ -1,14 +1,44 @@
-package org.halil.ibrahim.ozturk;
+package org.ozturk;
 
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello! This is `Live Football World Cup Score Board`!");
+        System.out.printf("\nHello! This is `Live Football World Cup Score Board`!\n");
 
+        //  Simulate use of `Live Football World Cup Score Board` Library
+        Scoreboard scoreboard = new Scoreboard();
 
+        // Start matches
+        scoreboard.startMatch("Turkiye", "Norway");
+        scoreboard.startMatch("Spain", "Brazil");
+        scoreboard.startMatch("Germany", "France");
+        scoreboard.startMatch("Uruguay", "Italy");
+        scoreboard.startMatch("Argentina", "Australia");
+
+        // Update scores
+        scoreboard.updateScore("Turkiye", "Norway", 0, 5);
+        scoreboard.updateScore("Spain", "Brazil", 10, 2);
+        scoreboard.updateScore("Germany", "France", 2, 2);
+        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
+        scoreboard.updateScore("Argentina", "Australia", 3, 1);
+
+        // Print summary
+        System.out.println("\n=== Match Summary ===");
+        List<Match> summary = scoreboard.getSummary();
+        for (Match match : summary) {
+            System.out.println(match.getHomeTeam() + " " + match.getHomeScore() + " - " +
+                    match.getAwayTeam() + " " + match.getAwayScore());
+        }
+
+        // Finish a match
+        scoreboard.finishMatch("Germany", "France");
+
+        System.out.println("\n=== After Finishing Germany vs France ===");
+        summary = scoreboard.getSummary();
+        for (Match match : summary) {
+            System.out.println(match.getHomeTeam() + " " + match.getHomeScore() + " - " +
+                    match.getAwayTeam() + " " + match.getAwayScore());
+        }
     }
 }
